425fee967b78efdc7d6641be9ed118ef15a5d683
wanfrev <wanfrevargas@gmail.com>
2026-06-27 12:31:37 -0400

    CAMBIO

--- client/src/queryClient.ts
+++ client/src/queryClient.ts
@@ -41,7 +41,7 @@ export const queryClient = new QueryClient({
   }),
   defaultOptions: {
     queries: {
-      staleTime: 5 * 60 * 1000,
+      staleTime: 0,
       gcTime: oneDayMs,
       retry: 1,
       refetchOnWindowFocus: true,
